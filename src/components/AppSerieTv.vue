b79baef219caeb8b95defb41a0087c5aa705bcb6
Marco <marcoregina92@gmail.com>
2023-12-06 18:10:08 +0100

    add poster serie tv

--- src/components/AppSerieTv.vue
+++ src/components/AppSerieTv.vue
@@ -24,12 +24,13 @@ export default {
     <div class="singleCard">
 
         <!-- <h3>Titolo: {{ tv.name }}</h3>
-                <h5>Titolo originale:{{ tv.original_name }}</h5>
-                <img v-if="store.flag.includes(tv.original_language)" :src="'/' + tv.original_language + '.png'" class="flag"
-                    alt="">
-                <h5 v-else> {{ tv.original_language }}</h5>
-                <h5>Voto: {{ tv.vote_average }}</h5> -->
-        <img :src="this.store.poster + tv.poster_path" alt="">
+            <h5>Titolo originale:{{ tv.original_name }}</h5>
+            <img v-if="store.flag.includes(tv.original_language)" :src="'/' + tv.original_language + '.png'" class="flag"
+                alt="">
+            <h5 v-else> {{ tv.original_language }}</h5>
+            <h5>Voto: {{ tv.vote_average }}</h5> -->
+        <img v-if="tv.poster_path" :src="this.store.poster + tv.poster_path" alt="">
+        <h3 v-else class="text-center d-flex"> NESSUNA IMMAGINE</h3>
 
 
     </div>
