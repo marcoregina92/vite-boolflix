ffb2ca2f2f7e26cac601247b39947947858b380c
Marco <marcoregina92@gmail.com>
2023-12-07 11:06:53 +0100

    sistema bug fontawesome e trasforma voto in stelle

--- src/components/AppMovie.vue
+++ src/components/AppMovie.vue
@@ -25,15 +25,53 @@ export default {
 
 <template>
     <div class="singleCard">
-        <!-- <img v-if="movie.poster_path" :src="this.store.poster + movie.poster_path" alt="">
-                    <h3 v-else class="text-center"> NESSUNA IMMAGINE DISPONIBILE</h3> -->
+        <img v-if="movie.poster_path" :src="this.store.poster + movie.poster_path" alt="">
+        <h3 v-else class="text-center"> NESSUNA IMMAGINE DISPONIBILE</h3>
 
         <h3>Titolo: {{ movie.title }}</h3>
         <h5>Titolo originale:{{ movie.original_title }}</h5>
         <img v-if="store.flag.includes(movie.original_language)" :src="'/' + movie.original_language + '.png'" class="flag"
             alt="">
         <h5 v-else> {{ this.movie.original_language }} </h5>
-        <h5>Voto: {{ Math.round(movie.vote_average / 2) }}</h5>
+
+        <div v-if="(Math.round(movie.vote_average / 2)) == 0" class="">
+            <h5>Voto: 0</h5>
+        </div>
+
+        <span v-if="(Math.round(movie.vote_average / 2)) == 1" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(movie.vote_average / 2)) == 2" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(movie.vote_average / 2)) == 3" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(movie.vote_average / 2)) == 4" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(movie.vote_average / 2)) == 5" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
     </div>
 </template>
 
@@ -51,6 +89,10 @@ export default {
 
 img {
     width: 100%;
-    height: 100%;
+    /* height: 100%; */
+}
+
+.gold {
+    color: gold;
 }
 </style>
--- src/components/AppSerieTv.vue
+++ src/components/AppSerieTv.vue
@@ -22,16 +22,52 @@ export default {
 
 <template>
     <div class="singleCard">
+        <img v-if="tv.poster_path" :src="this.store.poster + tv.poster_path" alt="">
+        <h3 v-else class="text-center"> NESSUNA IMMAGINE DISPONIBILE</h3>
 
         <h3>Titolo: {{ tv.name }}</h3>
         <h5>Titolo originale:{{ tv.original_name }}</h5>
         <img v-if="store.flag.includes(tv.original_language)" :src="'/' + tv.original_language + '.png'" class="flag"
             alt="">
         <h5 v-else> {{ tv.original_language }}</h5>
-        <h5>Voto: {{ Math.round(tv.vote_average / 2) }}</h5>
-        <!-- <img v-if="tv.poster_path" :src="this.store.poster + tv.poster_path" alt="">
-                            <h3 v-else class="text-center d-flex"> NESSUNA IMMAGINE DISPONIBILE</h3> -->
+        <div v-if="(Math.round(tv.vote_average / 2)) == 0" class="">
+            <h5>Voto: 0</h5>
+        </div>
 
+        <span v-if="(Math.round(tv.vote_average / 2)) == 1" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(tv.vote_average / 2)) == 2" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(tv.vote_average / 2)) == 3" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(tv.vote_average / 2)) == 4" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
+
+        <span v-if="(Math.round(tv.vote_average / 2)) == 5" class="">
+            <h5>Voto:</h5>
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+            <font-awesome-icon :icon="['fas', 'star']" class="gold" />
+        </span>
 
     </div>
 </template>
@@ -51,6 +87,10 @@ export default {
 
 img {
     width: 100%;
-    height: 100%;
+    /* height: 100%; */
+}
+
+.gold {
+    color: gold;
 }
 </style>
